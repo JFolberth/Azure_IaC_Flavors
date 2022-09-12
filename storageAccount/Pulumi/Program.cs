4d59ed3beab9dbc1c41ff882744649d0d816e7fe
JFolberth <johnfolberth@gmail.com>
2022-09-11 22:51:59 -0400

    Feature/pulumi/storageaccount (#9)
    * Adding code for Pulumi storage account
    
    * pulumi code

---
+++ storageAccount/Pulumi/Program.cs
@@ -0,0 +1,40 @@
+using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    var config = new Pulumi.Config();
+    var regionReference = new Dictionary<string, string>();
+    regionReference.Add("centralus", "cus");
+    regionReference.Add("eastus", "eus");
+    regionReference.Add("westus", "wus");
+    regionReference.Add("westus2", "wus2");
+
+
+    var suffix = config.Require("baseName") + "-" + config.Get("env") + "-" + regionReference[config.Require("location")];
+    var resourceGroupName = "rg-" + suffix;
+
+    // Create an Azure Resource Group
+    var resourceGroup = new ResourceGroup("resourceGroup", new ResourceGroupArgs
+    {
+        Location = config.Require("location"),
+        ResourceGroupName = resourceGroupName
+    });
+
+    // Create an Azure resource (Storage Account)
+    var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        AccountName = "sa" + suffix.Replace("-", "").ToLower(),
+        Location = resourceGroup.Location,
+        Sku = new SkuArgs
+        {
+            Name = config.Require("storageAccountSKU")
+        },
+        Kind = Kind.StorageV2
+    });
+
+});
